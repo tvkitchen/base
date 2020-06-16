ff303e365ed87699c5e09509530f14134520ae25
Dan Schultz <slifty@gmail.com>
2020-06-16 15:05:02 -0400

    Add log level constants
    As we seek to have consistant logging across the application, these log
    levels should make it easier for Appliance developers to log
    appropriately.
    
    Issue #50

---
+++ packages/constants/src/logLevels.js
@@ -0,0 +1,7 @@
+// Note: Winston log levels are camelCase, so we match that here
+export const fatal = 'fatal'
+export const error = 'error'
+export const warn = 'warn'
+export const info = 'info'
+export const debug = 'debug'
+export const trace = 'trace'
