6516557210ef17c055f5ac5b46357325d03e282d
Dan Schultz <slifty@gmail.com>
2020-06-16 14:26:22 -0400

    Add logger to IAppliance
    Appliances are going to need to be able to log things, especially as we
    work towards the concept of a method that verifies if an Appliance has
    dependencies properly installed and outputs resolution instructions.
    
    This adds a logger with an API that will work with
    a [winston](https://www.npmjs.com/package/winston)  object.
    
    There are methods for each of the log levels specified in the TV Kitchen
    base constant `logLevel`.
    
    Appliance developers should not assume that logger has been replaced,
    and should only call the methods that are officially supported in the
    IAppliance documentation.
    
    Issue #48

--- packages/interfaces/src/IAppliance.js
+++ packages/interfaces/src/IAppliance.js
@@ -2,8 +2,22 @@ import {
 	InterfaceInstantiationError,
 	NotImplementedError,
 } from '@tvkitchen/base-errors'
+import {
+	logLevels,
+} from '@tvkitchen/base-constants'
 
 class IAppliance {
+	// A simple, replaceable logger for use by the Appliance.
+	logger = {
+		log: (level, message) => console.log(`${level}: ${message}`), // eslint-disable-line no-console
+		fatal: (message) => this.logger.log(logLevels.fatal, message),
+		error: (message) => this.logger.log(logLevels.error, message),
+		warn: (message) => this.logger.log(logLevels.warn, message),
+		info: (message) => this.logger.log(logLevels.info, message),
+		debug: (message) => this.logger.log(logLevels.debug, message),
+		trace: (message) => this.logger.log(logLevels.trace, message),
+	}
+
 	/**
 	 * Constructor for a new appliance.
 	 *
